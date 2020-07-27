69f42120a45adc866b7f6e9b2f7040b18b6fc6c9
Juan Casaboza <jcasaboza@hotmail.com>
2020-07-27 00:58:44 -0500

    First commit of the test, hope it works.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+##Dada la complejidad de calcular la inversa de la matriz
+##se suele beneficiar de capturar el cache de la ya resuelta
 
-## Write a short comment describing this function
+##Given that usually the computation of a matrix inversion is complex
+##we could benefit from capturing the inverse rather than computing it
+##every time
+
+##My native language is spanish, so most of my variables
+##are related to my language
 
 makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  conf <- function(y){
+    inv <<- NULL
+    x <<- y
+  }
+  tom <- function()x
+  confinv <- function(inversa) inv <<- inversa
+  tominv <- function() inv
+  list(conf = conf, tom = tom, confinv = confinv, tominv= tominv)
 
 }
 
 
-## Write a short comment describing this function
+## inv comes from inversa that translates to inverse
+##tom comes from tomar algo that translates to get something
+## conf comes from configurar as in darle un valor a algo 
+## So, what I did was change the function that takes the mean 
+## which they gave us as the example and rerun it as the inv
+## This way, it does the same, except that it solves the matrix 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv <- x$tominv()
+  if(!is.null(inv)){
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$tom()
+  inv <- solve(data)
+  x$confinv(inv)
+  inv
 }
